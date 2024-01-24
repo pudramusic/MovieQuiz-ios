61057ecc611ddf67f7b6992bd14f5f7b9ea34df4
pudramusic <flyandseeru@gmail.com>
2024-01-24 12:14:26 +0300

    делегирование зависимостей и реализация паттерна делегат для фабрики

---
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryDelegate.swift
+//  MovieQuiz
+//
+//  Created by Yo on 23/1/24.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Yo on 22/1/24.
+//
+
+import Foundation
