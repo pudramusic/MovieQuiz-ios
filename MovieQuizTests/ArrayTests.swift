089c46390762931e93d8792c1eb37e8b3da6cd2e
pudramusic <flyandseeru@gmail.com>
2024-02-24 09:16:32 +0300

    MovieQuizTests MovieLoaderTests

---
+++ MovieQuizTests/ArrayTests.swift
@@ -0,0 +1,8 @@
+//
+//  ArrayTests.swift
+//  MovieQuizTests
+//
+//  Created by Yo on 24/2/24.
+//
+
+import Foundation
---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Yo on 24/2/24.
+//
+
+import Foundation
